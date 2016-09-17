aa09f4e1a494206e44f95de8926aa9ca18be98a0
Brian lee <Brian lee>
2016-09-17 16:12:58 -0700

    add main page xaml

---
+++ XaDemo/XaDemo/View/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XaDemo.View
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+            FindBtn.Clicked += OnFindBtnClicked;
+		}
+        private async void OnFindBtnClicked(object sender, EventArgs e)
+        {
+            // Page appearance not animated
+            await Navigation.PushAsync(new RestaurantLayout(), true);
+        }
+    }
+}
