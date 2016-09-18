6f04d556dc58f329f1ae4f9187254595bd7acd0d
Brian Lee <Brian.Lee@studentpartner.com>
2016-09-19 05:20:37 +0800

    add Id Json property

--- XaDemo/XaDemo/Data/Restaurant.cs
+++ XaDemo/XaDemo/Data/Restaurant.cs
@@ -8,6 +8,7 @@ namespace XaDemo.Data
 {
     class Restaurant
     {
+        [JsonProperty(PropertyName = "Id")]
         public string Id { get; private set; }
         [JsonProperty(PropertyName = "Name")]
         public string Name { get; private set; }
